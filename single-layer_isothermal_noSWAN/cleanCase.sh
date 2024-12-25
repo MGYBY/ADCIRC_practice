00e7fc0f07700fc89465d6aede554516b9991a3b
BYY <53412286+MGYBY@users.noreply.github.com>
2024-12-24 23:43:15 -0500

    Update cleanCase.sh

--- single-layer_isothermal_noSWAN/cleanCase.sh
+++ single-layer_isothermal_noSWAN/cleanCase.sh
@@ -1,4 +1,5 @@
-rm fort.16 fort.33 fort.63 fort.64 fort.73 fort.74 dataset.h5
-rm fort.16 fort.33 fort.63 fort.64 fort.73 fort.74 dataset.h5
+rm fort.16 fort.33 fort.63 fort.64 fort.73 fort.74 dataset.h5 *.63
+rm fort.67 fort.68 fort.80
 rm -rf PE0*
 rm partmesh.txt metis_graph.txt
+rm *.nc
